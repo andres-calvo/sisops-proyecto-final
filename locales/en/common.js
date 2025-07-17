88f0b0f877eb4af078e8f83d284125c38983597f
Andres Calvo <calvo.andres.tech@gmail.com>
2025-07-17 01:12:15 -0500

    Update again translations

--- locales/en/common.js
+++ locales/en/common.js
@@ -1,4 +1,7 @@
 export default {
   "nav.todo": "ToDo",
-  "language-switcher": "Change language"
+  "language-switcher": "Change language",
+  "dummy_key1": "Dummy Value 1",
+  "dummy_key2": "Dummy Value 2",
+  "dummy_key3": "Dummy Value 3",
 }; 
--- locales/es/common.js
+++ locales/es/common.js
@@ -1,4 +1,7 @@
 export default {
   "nav.todo": "ToDo",
-  "language-switcher": "Cambiar idioma"
+  "language-switcher": "Cambiar idioma",
+  "dummy_key1": "Valor Dummie 1",
+  "dummy_key2": "Valor Dummie 2",
+  "dummy_key3": "Valor Dummie 3",
 }; 
